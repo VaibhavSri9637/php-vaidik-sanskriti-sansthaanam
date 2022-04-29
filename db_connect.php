693de14aa8443bc6a09a9eecbfb05ea96a6985e8
Vaibhav Srivastava <vaibhavsri9637@gmail.com>
2022-04-29 11:02:27 +0530

    updating database config

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', 'nf16Mysql', 'vaidic_sanskriti_sansthaanam');
+$conn = mysqli_connect('localhost', 'root', {db_pass}, 'vaidic_sanskriti_sansthaanam');
 mysqli_set_charset($conn, 'utf8');
 if (!$conn) {
     echo 'Connection error: ' . mysqli_connect_error();
